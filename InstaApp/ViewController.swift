5b23c60afbedbc6fccb472fdedc0338ba62343f7
GitHub <abduqahhor2349@gmail.com>
2022-01-17 17:27:26 +0500

    Initial Commit

---
+++ InstaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaApp
+//
+//  Created by Abduqaxxor on 17/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
